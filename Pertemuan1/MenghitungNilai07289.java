028ce7d7caac5edeebadfe3e3b99a21a95402550
hirdhan <hirdhanantama46@gmail.com>
2021-10-27 09:59:54 +0700

    Pertemuan 1

--- Pertemuan1/MenghitungNilai07289.java
+++ Pertemuan1/MenghitungNilai07289.java
@@ -1,41 +1,34 @@
-package MenghitungNilai07289;
+package Pertemuan1;
 
 import java.util.Scanner;
 
 public class MenghitungNilai07289 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double rata, total = 0, nilai;
-        String nama;
+        Scanner input07289 = new Scanner(System.in);
+        double rata07289, total07289 = 0, nilai07289;
+        int i=1, j=1;
+        String nama07289;
 
         System.out.print("Masukkan Jumlah Mata Kuliah: ");
-        int jumlah = input.nextInt();
+        int jumlah07289 = input07289.nextInt();
         System.out.println();
 
-        for(int i=1; i<=jumlah; i++ ){
-            System.out.print("Nama Mata Kuliah ke  - "+ i+ ": ");
-            nama = input.next();
-            System.out.print("Nilai Mata Kuliah ke- "+i+ ": ");
-            nilai = input.nextDouble();
-            total +=  nilai;
+        do {
+            System.out.print("Nama Mata Kuliah ke -" + j + " : ");
+            nama07289 = input07289.next();
+            System.out.print("Nilai Mata Kuliah ke -" + j + " : ");
+            nilai07289 = input07289.nextDouble();
+            System.out.println();
+            total07289 += nilai07289;
+            i++;
+            j++;
         }
+        while (i <= jumlah07289);
 
-//        do {
-//            System.out.print("Nama Mata Kuliah ke -" + j + " : ");
-//            nama = input.next();
-//            System.out.print("Nilai Mata Kuliah ke -" + j + " : ");
-//            nilai = input.nextDouble();
-//            System.out.println();
-//            total += nilai;
-//            i++;
-//            j++;
-//        }
-//        while (i <= jumlah);
+        rata07289 = total07289 / jumlah07289;
+        System.out.println("Nilai Rata-rata : " + rata07289);
 
-        rata = total / jumlah;
-        System.out.println("Nilai Rata-rata : " + rata);
-
-        if (rata >= 71) {
+        if (rata07289 >= 71) {
             System.out.println("Keterangan : LULUS");
         } else {
             System.out.println("Keterangan : GUGUR");
